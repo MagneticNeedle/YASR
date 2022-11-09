2b630e2ab29a5b194590a66fb1d7d91cd7eaab73
p0lygun <solankivibhakar82@gmail.com>
2022-11-10 00:14:17 +0530

    speed: pre calc count(1)

--- leetcode/2125.py
+++ leetcode/2125.py
@@ -1,9 +1,9 @@
 # https://leetcode.com/problems/number-of-laser-beams-in-a-bank/description/
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        bank = [_ for _ in bank if "1" in _] # we only care for rows that contain devices
+        bank = [_.count("1") for _ in bank if "1" in _]
         nb = 0
         for i,r in enumerate(bank):
             if (n:=i+1)<len(bank):
-                nb += r.count('1') * bank[n].count('1')  # just do a product :) 
+                nb += r * bank[n]
         return nb
