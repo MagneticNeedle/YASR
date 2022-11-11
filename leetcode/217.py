f9034f078191045eff9fb27f7dd57ef1036d95c7
p0lygun <solankivibhakar82@gmail.com>
2022-11-12 01:46:55 +0530

    solve: p217 - contains-duplicate

---
+++ leetcode/217.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/contains-duplicate/
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+print(Solution().containsDuplicate([1,2,3,4]))
