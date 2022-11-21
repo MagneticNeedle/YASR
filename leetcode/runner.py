86d4980d7a15dcf0a2c9ea122cd8259a6d3b83e4
p0lygun <solankivibhakar82@gmail.com>
2022-11-21 23:01:46 +0530

    feat: reset to default state

--- leetcode/runner.py
+++ leetcode/runner.py
@@ -9,7 +9,4 @@ if __name__ == '__main__':
     solution_obj = importlib.import_module(last_modified.stem).Solution()
     functions = [i for i in dir(solution_obj) if "__" not in i and callable(getattr(solution_obj, i))]
     if functions:
-        print(getattr(solution_obj, next(iter(functions)))(
-            [[".","+"]]
-            , [0,0]
-        ))
+        print(getattr(solution_obj, next(iter(functions)))())
