c5f7d641faa8c949eba9d12ab628a4de4020f233
p0lygun <solankivibhakar82@gmail.com>
2022-11-21 23:00:33 +0530

    feat: improve handling got selecting main function

--- leetcode/runner.py
+++ leetcode/runner.py
@@ -7,6 +7,9 @@ last_modified = max([file for file in Path.cwd().glob("*.py") if file.stem != "r
 
 if __name__ == '__main__':
     solution_obj = importlib.import_module(last_modified.stem).Solution()
-    functions = [i for i in dir(solution_obj) if not i.startswith("__")]
+    functions = [i for i in dir(solution_obj) if "__" not in i and callable(getattr(solution_obj, i))]
     if functions:
-        getattr(solution_obj, next(iter(functions)))([], [])
+        print(getattr(solution_obj, next(iter(functions)))(
+            [[".","+"]]
+            , [0,0]
+        ))
