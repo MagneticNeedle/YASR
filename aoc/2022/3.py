c2109d714de6fab56f013b54ee87d623b9941e36
p0lygun <solankivibhakar82@gmail.com>
2022-12-04 11:01:56 +0530

    solve:aoc: d3,d4

---
+++ aoc/2022/3.py
@@ -0,0 +1,19 @@
+from ..aoc import get_input
+from sortedcontainers import SortedList, SortedSet
+from collections import defaultdict, Counter
+from itertools import chain
+from string import ascii_letters
+
+day_input =  get_input(3).strip().split("\n")
+print(
+    sum(
+        [
+            ascii_letters.index(j) + 1
+            for i in range(0, len(day_input), 3)
+            for j in set(day_input[i]).intersection(
+                day_input[i + 1], day_input[i + 2]
+            )
+        ]
+    )
+)
+# is copilot on ?  
---
+++ aoc/2022/4.py
@@ -0,0 +1,13 @@
+# this script is a solution to Advent of Code 2022, day 4
+
+from ..aoc import get_input
+
+day_input = get_input(4).split('\n')
+total_items = len(day_input)
+m = 0
+for i in range(total_items):
+    l,r = (list(map(int,i.split('-'))) for i in day_input[i].split(','))
+    if r[0] <= l[1] and r[1] >= l[0]:
+        m += 1
+print(m)
+
