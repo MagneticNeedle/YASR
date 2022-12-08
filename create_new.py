a274ca6bd81a54cd9bc85e52e5450e0e2c53ef95
p0lygun <solankivibhakar82@gmail.com>
2022-12-08 22:34:42 +0530

    feat: add aoc to create_new script

--- create_new.py
+++ create_new.py
@@ -1,5 +1,8 @@
 import sys
 from pathlib import Path
+
+base_path = Path(__file__).parent
+
 base_file = """
 from typing import List
 from collections import defaultdict, Counter
@@ -8,6 +11,12 @@ from math import inf
 import bisect
 
 \n""".lstrip()
+
+aoc_specific = """
+from ..aoc import get_input
+
+day_input =  get_input(3).strip()
+"""
 
 first = sys.argv[1]
 second = None if len(sys.argv) <= 2 else sys.argv[2]
@@ -24,16 +33,16 @@ else:
         case 'l':
             site = "leetcode"
         case 'a':
+            with open(base_path / 'aoc' / 'year.txt') as year:
+                site = "aoc/" + year.read().strip()
+        case _:
+            print("Unkown site, exiting....")
             exit(1)
 
-if site:
-    file_path = Path(__file__).parent / site / f'{first}.py'
-    if file_path.exists():
-        print(f"{file_path} exists")
-        exit(0)
-    with file_path.open('w') as file:
-        file.write(base_file)
-    print(f'{file_path} Created')
-else:
-    print('Unknown site provided')
-    exit(1)
+file_path = Path(__file__).parent / site / f'{first}.py'
+if file_path.exists():
+    print(f"{file_path} exists")
+    exit(0)
+with file_path.open('w') as file:
+    file.write(base_file)
+print(f'{file_path} Created')
