bd41670579e0b528bb77f20acfb4208e8213b076
p0lygun <solankivibhakar82@gmail.com>
2023-12-04 11:44:48 +0530

    feat(AoC:2023) add solution for day 4

---
+++ aoc/2023/4.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import defaultdict, Counter
+from queue import Queue
+from math import inf
+import bisect
+import string
+import re
+from math import *
+from ..aoc_api import get_input
+
+day_input = get_input(4).strip().splitlines()
+
+
+def get_data(line: str):
+    c_id = int(line.split(':')[0].split()[-1].strip())
+    parts = line.split(':')[1].split('|')
+    winnings = set(map(int, parts[0].split()))
+    c_nums = set(map(int, parts[1].split()))
+    wins = len(winnings.intersection(c_nums))
+
+    return c_id, wins
+
+
+CARDS_COUNT = {card_id: 1 for card_id in range(1, len(day_input) + 1)}
+total = 0
+
+for line in day_input:
+    card_id, number_of_winning_cards = get_data(line)
+
+    # part one
+    total += int(2 ** (number_of_winning_cards - 1))
+
+    # part two
+    for i in range(card_id + 1, card_id + number_of_winning_cards + 1):
+        CARDS_COUNT[i] += 1 * CARDS_COUNT[card_id]
+
+print(total)
+print(sum(CARDS_COUNT.values()))
